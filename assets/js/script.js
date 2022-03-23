84d26aed3ce9e6b25b2a8d8df88c3320663c1d2c
llattanzi <lauren71442@gmail.com>
2022-03-22 21:34:38 -0400

    add html for schedule. Add current time to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,44 @@
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
+
+var setHeaderDate = function() {
+    var currentDate = moment().format("MMM DD, YYYY");
+    $("#currentDay").text(currentDate);
+}
+
+var taskColors = function() {
+    var currentHour = moment().format("HH");
+    currentHour = JSON.parse(currentHour);
+    var numHours = $(".time-block").length;
+
+    for (i = 0; i < numHours; i++) {
+        var hour = $(".hour p")[i].textContent;
+        // if the hour is PM, add 12 hours to the number to match current time HH format
+        if (hour.includes("P.M.")) {
+            var add = 12;
+        }
+        else {
+            // if A.M. do not add hours
+            var add = 0;
+        }
+        // extract only the number from the hour text
+        hour = hour.substring(0,2).trim();
+        // convert to number
+        hour = JSON.parse(hour);
+        hour = hour + add;
+        console.log(hour);
+        if (hour < currentHour) {
+            $(".description")[i].addClass("past");
+        }
+        else if (hour == currentHour) {
+            $(".description")[i].addClass("current");
+        }
+        else if (hour > currentHour) {
+            $(".description")[i].addClass("future");
+        }
+    }
+}
+
+setHeaderDate();
+taskColors();
